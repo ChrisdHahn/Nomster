89edf7e58cd8ba50aae432b8d20aa5802b43bfef
Chris Hahn <chrishahnd@gmail.com>
2017-05-15 04:11:53 +0000

    Halfway through image challenge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514010219) do
+ActiveRecord::Schema.define(version: 20170515035826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,12 @@ ActiveRecord::Schema.define(version: 20170514010219) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.text     "caption"
+    t.integer  "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id", using: :btree
   end
 
   create_table "places", force: :cascade do |t|
